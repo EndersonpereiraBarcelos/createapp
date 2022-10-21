7dcc719120dc66cff545945bc1c3ad842ad74e9c
endersonpb <souza98120@gmail.com>
2022-10-21 16:25:58 -0300

    useEffect

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Home from "./pages/Home";
+import "./styles/global.css";
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>
-)
+);
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,51 @@
+import "./style.css";
+import React, { useState, useEffect } from "react";
+import Card from "../../components/Card/index";
+
+export default function Home() {
+  const [studentName, setStudentName] = useState();
+
+  const [students, setStudents] = useState([]);
+
+  function handleAddStudent() {
+    const newStudent = {
+      name: studentName,
+      time: new Date().toLocaleTimeString("pt-br", {
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+      }),
+    };
+
+    setStudents((prevState) => [...prevState, newStudent]);
+  }
+
+  useEffect(()=>{
+    console.log("useEffect foi chamado")
+  },[])
+
+
+
+  return (
+    <div className="container">
+      <header>
+      <h1>Lista de presença</h1>
+      <div>
+        <strong>Endersonpb</strong>
+        <img src="https://github.com/EndersonpereiraBarcelos.png"/>
+      </div>
+      </header>
+      <input
+        type="text"
+        placeholder="Digite o nome"
+        onChange={(e) => setStudentName(e.target.value)}
+      />
+      <button type="button" onClick={handleAddStudent}>
+        Adicionar
+      </button>
+      {students.map((student) => (
+        <Card key={student.time} name={student.name} time={student.time} />
+      ))}
+    </div>
+  );
+}
